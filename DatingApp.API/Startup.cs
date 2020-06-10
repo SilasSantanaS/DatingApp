bb79b5eb0221a10de2068d54b1faff1cedf925f1
Silas Santana <silasuper3@gmail.com>
2020-06-10 13:36:36 -0300

    Login and Signin error handling

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -16,6 +16,14 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 
+
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+using DatingApp.API.Helpers;
+
+
 namespace DatingApp.API
 {
     public class Startup
@@ -53,6 +61,24 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(builder => 
+                {
+                    builder.Run(async context => 
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+            }
+    
 
             ///app.UseHttpsRedirection();
 
