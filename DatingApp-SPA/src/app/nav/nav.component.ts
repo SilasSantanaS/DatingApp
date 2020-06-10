e448430b2e7eb62119880ef5c187dc82a9b8af2e
Silas Santana <silasuper3@gmail.com>
2020-06-10 13:40:36 -0300

    Login and Signin error handling 2

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log("Usuário logado com sucesso.");
     }, error => {
-      console.log("Falha ao logar usuário");
+      console.log(error);
     });
   }
 
